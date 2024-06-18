3fd3dc88fc5c6ccc3f72dff2e094bcb833e98df8
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-06-18 23:50:46 +0200

    Partialy realize minimalistic ui.

---
+++ modules/tui.py
@@ -0,0 +1,61 @@
+import curses
+import re
+
+from typing import List
+
+from modules.process.core import Process
+
+
+class UI:
+    def __init__(self):
+        self._screen = curses.initscr()
+        self._height, self._width = self._screen.getmaxyx()
+
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(False)
+        curses.start_color()
+        curses.use_default_colors()
+        curses.init_pair(
+            curses.COLOR_RED,
+            curses.COLOR_RED,
+            curses.COLOR_BLACK
+        )
+        curses.init_pair(
+            curses.COLOR_GREEN,
+            curses.COLOR_GREEN,
+            curses.COLOR_BLACK,
+        )
+        curses.init_pair(
+            curses.COLOR_YELLOW,
+            curses.COLOR_YELLOW,
+            curses.COLOR_BLACK,
+        )
+        # TODO: Setup rest of colors
+        self._screen.keypad(True)
+
+
+    def display_processes(self, processes: List[Process]):
+        self._screen.clear()
+
+        for process in processes:
+            self._addstr(str(process))
+
+        self._screen.addstr(int(self._height - 1), 0, "PRESS CTRL+C TO TERMINATE", curses.A_REVERSE)
+        self._screen.refresh()
+
+    def _addstr(self, str_: str):
+        colors = {
+            "CREATED": curses.COLOR_WHITE,
+            "READY": curses.COLOR_RED,
+            "WAITING": curses.COLOR_CYAN,
+            "EXECUTING": curses.COLOR_YELLOW,
+            "FINISHED": curses.COLOR_GREEN,
+        }
+        pattern: str = r'({0:s})'.format(
+                '|'.join(r'\b{0:s}\b'.format(word) for word in colors.keys())
+                )
+        chars: list[str] = re.split(pattern, str_)
+        for c in chars:
+            self._screen.addstr(c,curses.color_pair(colors.get(c, 0)))
+
