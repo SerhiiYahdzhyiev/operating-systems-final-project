d587cafcbdf482c32e079f68a873b67f48cf0f32
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-06-25 21:55:17 +0200

    Simplify and realize minimalistic argument parsing.

--- src/pssim/modules/args.py
+++ src/pssim/modules/args.py
@@ -1,11 +1,8 @@
 from argparse import ArgumentParser
 
 root_parser = ArgumentParser(
-        prog="pssim",
-        description="Processes Scheduling and Memory Management Simulator",
-        )
+    prog="pssim",
+    description="Process Scheduling and Memeory Allocation Simulator"
+)
 
-root_parser.add_argument("command")
-command_parsers = root_parser.add_subparsers(title="commands", dest="command")
-
-run_parser = command_parsers.add_parser("run", help="Runs the simulation")
+root_parser.add_argument("command", choices=["run", "show-config"])
