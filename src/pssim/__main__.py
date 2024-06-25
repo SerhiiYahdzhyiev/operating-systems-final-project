b16eb9b0578b3caab0e32349ec5d6edd080d4325
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-06-25 22:17:22 +0200

    Update entry point.

--- src/pssim/__main__.py
+++ src/pssim/__main__.py
@@ -6,26 +6,26 @@ from pssim.modules.args import root_parser
 from pssim.modules.config import show_config
 from pssim.modules.simenv import SimulationEvironment
 
-simenv = SimulationEvironment()
 
-# TODO: Decompose, customize, rewrite, refactor...
 async def main():
     # TODO: Realize proper mechanism to config the simulation
     args = root_parser.parse_args()
 
     if args.command == "show-config":
         show_config()
-        exit(0)
 
     if args.command == "run":
+        simenv = SimulationEvironment()
         await simenv.run()
 
 def run_main():
     try:
         run(main())
     except KeyboardInterrupt:
-        curses.endwin()
         # TODO: Extend gracefull exit (if needed)
+        curses.endwin()
+        print("Exitting...")
+        exit(0)
 
 if __name__ == "__main__":
     run_main()
