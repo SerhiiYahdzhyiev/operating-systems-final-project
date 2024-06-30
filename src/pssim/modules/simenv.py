f3e963bb29a98f9339e03ac7b2a91c81d6cff452
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-06-30 21:57:06 +0200

    Update main run function in simulation environment class.

--- src/pssim/modules/simenv.py
+++ src/pssim/modules/simenv.py
@@ -1,4 +1,3 @@
-import asyncio
 from asyncio import sleep
 from pssim.interfaces.scheduler import ICpu
 from pssim.modules.config import sim_config as config
@@ -7,6 +6,7 @@ from pssim.modules.scheduler.core import Scheduler
 from pssim.modules.tui import UI
 from pssim.interfaces.process import IProcess
 from pssim.modules.scheduler.strategy import get_scheduling_strategy
+
 
 class CPU(ICpu):
     _cycle_time = 1
@@ -21,16 +21,44 @@ class CPU(ICpu):
 
 class SimulationEvironment:
     _num_processes = config["num_processes"] 
+    _timer = 0
 
     def __init__(self):
         self.ui = UI()
-        _scheduling_strategy = get_scheduling_strategy[config["scheduling_strategy"]](self.ui)
+        _scheduling_strategy = get_scheduling_strategy[config["scheduling_strategy"]]()
         self.scheduler = Scheduler(_scheduling_strategy)
         self.cpu = CPU()
 
     async def run(self): 
-        processes = []
+        _processes = []
         for _ in range(self._num_processes):
-            processes.append(ProcessFactory.create())
+            _processes.append(ProcessFactory.create())
 
-        await self.scheduler.schedule(processes, self.cpu, None, None)
+        arrives = {}
+
+        for process in _processes:
+            if not arrives.get(process.arrival_time, False):
+                arrives[process.arrival_time] = [process]
+            else:
+                arrives[process.arrival_time].append(process)
+
+        processes = []
+
+        while True:
+            arrived = arrives.get(self._timer, None)
+
+            if arrived:
+                for process in arrived:
+                    processes.append(process)
+                self.scheduler.schedule(arrived)
+
+            current = self.scheduler.get_current()
+
+            if current:
+                self.cpu.execute(current)
+
+
+            self.scheduler.update()
+            self._timer+=1
+            self.ui.update(self._timer, processes)
+            await sleep(self.cpu.cycle_time)
