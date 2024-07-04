ea16e21df9dec8ff10a80dc012f8755795e57a9b
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-07-04 15:55:08 +0200

    Remove unused files.

--- src/pssim/modules/memory/manager.py
+++
@@ -1,8 +0,0 @@
-from pssim.interfaces.memory import IMemeoryManager, IMemory, IMemoryChunk
-
-
-class MemeoryManager(IMemeoryManager):
-  # TODO: Realize
-  def alloc(self, ammount: int, memory: IMemory) -> IMemoryChunk: ...
-
-  def delloc(self, chunk: IMemoryChunk, memory: IMemory): ...
